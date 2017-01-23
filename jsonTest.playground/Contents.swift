5ed24f7cae1a02b59d9f848dcd42f0cc5dfdf656
B. Nelissen, MBP <b.g.l.nelissen@gmail.com>
2017-01-23 10:09:22 +0100

    initial commit

---
+++ jsonTest.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Goals
+
+// Get data from the server in an elegant way
+// - when collected, save data in dictionary
+// - When partitially collected, stop and redo the collection
+// - when no server is available, stop and tell the user
+
+// Sources and info used:
+// - developer.apple.com/swift/blog/?id=37
