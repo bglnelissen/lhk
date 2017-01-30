c3ebcfecbbc7e1b2a900bef889e5bdd4c54a9b37
B. Nelissen, Mini <b.g.l.nelissen@gmail.com>
2017-01-30 09:47:55 +0100

    restructre

--- git.sh
+++ git.sh
@@ -18,40 +18,42 @@ while [[ "" == "$commitmessage" ]]; do
   read -p "Commit message: " commitmessage
 done
 
-git status
+# git pull
+git pull pi master
 if [ 0 == $? ]; then
-  echo "Succes: git status"
-  echo "-------"
-  echo
-  git add *
-  if [ 0 == $? ]; then
-    echo "Succes: git add *"
-    echo "-------"
-    echo
-    git commit -m "$commitmessage"
-    if [ 0 == $? ]; then
-      echo "Succes: git commit -m "$commitmessage""
-      echo "-------"
-      echo
-      git push pi master
-      if [ 0 == $? ]; then
-        echo "Succes: git push pi master"
-        echo "-------"
-        echo
-      else
-        echo "FAIL: git push pi master"
-        exit 1
-      fi
-    else
-      echo "FAIL:git commit -m "$commitmessage""
-      exit 1
-    fi
-  else
-    echo "FAIL:git add *"
-    exit 1
-  fi
+  echo "Succes: git pull pi master"; echo "-------"; echo
 else
-  echo "FAIL: git status"
-  exit 1
+  echo "FAIL: git pull pi master"; exit 1
 fi
 
+# git status
+git status
+if [ 0 == $? ]; then
+  echo "Succes: git status"; echo "-------"; echo
+  git add *
+else
+  echo "FAIL: git status"; exit 1
+fi
+
+# git add
+if [ 0 == $? ]; then
+  echo "Succes: git add *"; echo "-------"; echo
+  git commit -m "$commitmessage"
+else
+  echo "FAIL:git add *"; exit 1
+fi
+
+# git commit
+if [ 0 == $? ]; then
+  echo "Succes: git commit -m "$commitmessage""; echo "-------"; echo
+  git push pi master
+else
+  echo "FAIL:git commit -m "$commitmessage""; exit 1
+fi
+
+# git push
+if [ 0 == $? ]; then
+  echo "Succes: git push pi master"; echo "-------"; echo
+else
+  echo "FAIL: git push pi master"; exit 1
+fi
