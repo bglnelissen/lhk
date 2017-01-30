72236bd5453af10c4a2528dae101b0265753cd94
B. Nelissen, Mini <b.g.l.nelissen@gmail.com>
2017-01-30 09:56:32 +0100

    debugging

--- git.sh
+++ git.sh
@@ -23,7 +23,7 @@ git pull pi master
 if [ 0 == $? ]; then
   echo "Succes: git pull pi master"; echo "-------"; echo
 else
-  echo "FAIL: git pull pi master";
+  echo "FAIL: git pull pi master"; exit 1
 fi
 
 # git status
@@ -32,7 +32,7 @@ if [ 0 == $? ]; then
   echo "Succes: git status"; echo "-------"; echo
   git add *
 else
-  echo "FAIL: git status";
+  echo "FAIL: git status"; exit 1
 fi
 
 # git add
@@ -41,7 +41,7 @@ if [ 0 == $? ]; then
   echo "Succes: git add *"; echo "-------"; echo
   git commit -m "$commitmessage"
 else
-  echo "FAIL: git add *";
+  echo "FAIL: git add *"; exit 1
 fi
 
 # git commit
@@ -50,7 +50,7 @@ if [ 0 == $? ]; then
   echo "Succes: git commit -a -m "$commitmessage""; echo "-------"; echo
   git push pi master
 else
-  echo "FAIL: git commit -a -m "$commitmessage"";
+  echo "FAIL: git commit -a -m "$commitmessage""; exit 1
 fi
 
 # git push
@@ -58,5 +58,5 @@ git push pi master
 if [ 0 == $? ]; then
   echo "Succes: git push pi master"; echo "-------"; echo
 else
-  echo "FAIL: git push pi master";
+  echo "FAIL: git push pi master"; exit 1
 fi
