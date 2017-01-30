561d5821b1ea5c687568f661ea638b445d56b301
B. Nelissen, Mini <b.g.l.nelissen@gmail.com>
2017-01-30 09:10:48 +0100

    bas restructure and cleanup

--- git.sh
+++ git.sh
@@ -5,14 +5,14 @@
 
 commitmessage="$@"
 
-# while [[ -z "$commitmessage" ]]; do
-  read -e -p "Commit message: " -i "$commitmessage" commitmessage
-# done
+while [[ -z "$commitmessage" ]]; do
+  read -e -p "Commit message: " commitmessage
+done
 
 
 git status
 gsexit=$?
-echo $gsexit
+
 if [ 0 != $gsexit ]; then
   echo "Problems with git, initialized?"
 else
